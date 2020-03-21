3de2fb37e1875b7ee54224fea5899f098c18e980
Łukasz Luty <rekrutacja@lutycorp.pl>
2020-03-21 13:58:53 +0100

    LL-1 create structure of crawler project

---
+++ src/main/java/com/wiprodigital/crawler/CrawlerAppApplication.java
@@ -0,0 +1,13 @@
+package com.wiprodigital.crawler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrawlerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrawlerAppApplication.class, args);
+	}
+
+}
