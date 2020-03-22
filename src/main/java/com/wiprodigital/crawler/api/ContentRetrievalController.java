c5037502d9263bc8c8ca144ce8df710a4730fee7
Łukasz Luty <rekrutacja@lutycorp.pl>
2020-03-22 17:55:14 +0100

    LL-5 Add swagger configuration

--- src/main/java/com/wiprodigital/crawler/api/ContentRetrievalController.java
+++ src/main/java/com/wiprodigital/crawler/api/ContentRetrievalController.java
@@ -4,6 +4,7 @@ package com.wiprodigital.crawler.api;
 import com.wiprodigital.crawler.domain.ContentRetrievalFacade;
 import com.wiprodigital.crawler.domain.CrawlingResult;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.net.MalformedURLException;
 import java.net.URL;
 
+@Slf4j
 @RestController
 @RequestMapping("/websites")
 @RequiredArgsConstructor
@@ -22,11 +24,16 @@ class ContentRetrievalController {
 
     @GetMapping
     public ResponseEntity<CrawlingResult> getExitAddresses(@RequestParam("url") String url,
-                                                           @RequestParam(value = "maxNestedCrawling", required = false) Integer maxNestedCrawling)
-            throws MalformedURLException {
+                                                           @RequestParam(value = "maxNestedCrawling", required = false) Integer maxNestedCrawling) {
         if (maxNestedCrawling != null && maxNestedCrawling < 0) {
             return ResponseEntity.badRequest().build();
         }
-        return ResponseEntity.ok(contentRetrievalFacade.retrieve(new URL(url), maxNestedCrawling));
+        try {
+            return ResponseEntity.ok(contentRetrievalFacade.retrieve(new URL(url), maxNestedCrawling));
+        } catch (MalformedURLException ex) {
+            log.warn("Cannot processing url [URL={}]", url, ex);
+            return ResponseEntity.badRequest().build();
+        }
+
     }
 }
--- src/main/java/com/wiprodigital/crawler/domain/ContentRetrievalFacade.java
+++ src/main/java/com/wiprodigital/crawler/domain/ContentRetrievalFacade.java
@@ -1,17 +1,20 @@
 package com.wiprodigital.crawler.domain;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
 
 @Service
+@Slf4j
 @RequiredArgsConstructor
 public class ContentRetrievalFacade {
 
     private final ContentRetrievalClient contentRetrievalClient;
 
     public CrawlingResult retrieve(URL url, Integer maxNestedCrawling) {
+        log.info("Start crawling process [URL = {}, maxNestedCrawling={}]", url.toString(), maxNestedCrawling);
         ContentParserCommand contentParserCommand = ContentParserCommand.builder()
                 .internalUrlPattern(url)
                 .retrievalProcessResult(contentRetrievalClient.retrieve(url, maxNestedCrawling))
